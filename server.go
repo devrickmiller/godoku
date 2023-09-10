f20b075505464797b5520028464648ec50188fdc
Rick Miller <dev.rick.miller@protonmail.com>
2023-09-10 09:52:04 -0700

    Initializing Project

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")	
+	})
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})	
+	})
+
+	httpPort := os.Getenv("PORT")
+	fmt.Println("PORT = ", httpPort)
+
+	if httpPort == "" { httpPort = "8080" }
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+	//e.Logger.Fatal(e.Start(":8080"))
+}
